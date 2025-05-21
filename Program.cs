a77e80fb2966b0069b6b60d366d5bf0fe1d5d076
tzirkus <zalmen5779@gmail.com>
2025-05-21 16:19:13 +0300

    first version zalmen

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Idf
+﻿using Idf.attack_units;
+using Idf.idf;
+namespace Idf
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            IdfMain a = new IdfMain("1948", "zamir");
+            AttackUnits b = new AttackUnits("f16", 56);
+            a.AddAttackUnit(b);
+            foreach (var i in a.AtackUnits)
+            {
+                Console.WriteLine(i.TypeUnit);
+                Console.WriteLine(i.FuelSupply);
+                Console.WriteLine(i.MaximumArmaments);
+                Console.WriteLine(i.CurrentAmmunitionQuantity);
+                Console.WriteLine(i.AttacksAtSameTime);
+                Console.WriteLine(i.PoweredBy);
+            }    
         }
     }
 }
--- obj/Debug/net8.0/Idf.AssemblyInfo.cs
+++ obj/Debug/net8.0/Idf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Idf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6651f337cf603e7b824d79c4c38df0019e1750be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Idf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Idf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
